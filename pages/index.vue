b73f054c4b02d6eac8450aad5adaa206b84ee40b
naasyakoot <naasyakoot@outlook.com>
2020-11-25 14:17:23 +0600

    First

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>SALAM</h1>
+</template>
+
+<script type="text/javascript">
+
+	export default {
+
+		head() {
+
+
+		return {
+
+			title: 'SALAM'
+		};
+
+
+	}
+
+
+	};
+	
+</script>
+
